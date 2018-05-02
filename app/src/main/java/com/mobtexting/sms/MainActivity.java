1ad57b2f75f2637738e53963f0226fcc55cf0fd2
Dhananjay Gupta <gupta.dhananjay59@gmail.com>
2018-05-02 14:45:10 +0530

    config sender id

--- app/src/main/java/com/mobtexting/sms/MainActivity.java
+++ app/src/main/java/com/mobtexting/sms/MainActivity.java
@@ -2,15 +2,32 @@ package com.mobtexting.sms;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.mobtexting.Mobtexting;
+import com.mobtexting.MobtextingInterface;
+import com.mobtexting.ModelError;
+import com.mobtexting.ServerResponse;
 
 
-
-public class MainActivity extends AppCompatActivity{
-
+public class MainActivity extends AppCompatActivity implements MobtextingInterface{
+    private Mobtexting mobtexting;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mobtexting=new Mobtexting(this);
+        mobtexting.sendSMS("This is a test","7250705072","Mobtext",this);
     }
 
+    @Override
+    public void onResponse(ServerResponse serverResponse) {
+        Log.d("response",serverResponse.getStatus()+"  "+serverResponse.getDescription());
+    }
+
+    @Override
+    public void onError(ModelError modelError) {
+        Log.d("response",modelError.getStatus()+"  "+modelError.getDescription());
+    }
 }
--- mobtextingsms/src/main/java/com/mobtexting/Mobtexting.java
+++ mobtextingsms/src/main/java/com/mobtexting/Mobtexting.java
@@ -24,63 +24,67 @@ public class Mobtexting {
     private static final String TAG = "Communicator";
     private static final String SERVER_URL = "http://api.mobtexting.com";
     private Retrofit retrofit;
-    private String api_key1;
+    private String api_key1,sender_id;
     private Context context;
 
     public Mobtexting(Context context){
         this.context=context;
     }
 
-    public void sendSMS(String message, String mobile_no, String senderId, final MobtextingInterface mobtextingInterface) {
+    public void sendSMS(String message, String mobile_no, final MobtextingInterface mobtextingInterface) {
         try {
             ApplicationInfo ai = context.getPackageManager().getApplicationInfo(context.getPackageName(), PackageManager.GET_META_DATA);
             Bundle bundle = ai.metaData;
             api_key1= bundle.getString("mobtexting.api_key");
+            sender_id= bundle.getString("mobtexting.sender_id");
         }catch (Exception e){
             mobtextingInterface.onError(new ModelError("100", "API key", "Dear developer. Don't forget to configure <meta-data android:name=\"mobtexting.api_key\" android:value=\"testValue\"/> in your AndroidManifest.xml file."));
             return;
         }
         if(api_key1!=null&&!api_key1.equals("")) {
+            if(sender_id!=null&&!sender_id.equals("")) {
+                HttpLoggingInterceptor logging = new HttpLoggingInterceptor();
+                logging.setLevel(HttpLoggingInterceptor.Level.BODY);
+                OkHttpClient.Builder httpClient = new OkHttpClient.Builder();
+                httpClient.addInterceptor(logging);
 
-            HttpLoggingInterceptor logging = new HttpLoggingInterceptor();
-            logging.setLevel(HttpLoggingInterceptor.Level.BODY);
-            OkHttpClient.Builder httpClient = new OkHttpClient.Builder();
-            httpClient.addInterceptor(logging);
+                retrofit = new Retrofit.Builder()
+                        .client(httpClient.build())
+                        .addConverterFactory(GsonConverterFactory.create())
+                        .baseUrl(SERVER_URL)
+                        .build();
 
-            retrofit = new Retrofit.Builder()
-                    .client(httpClient.build())
-                    .addConverterFactory(GsonConverterFactory.create())
-                    .baseUrl(SERVER_URL)
-                    .build();
+                Interface service = retrofit.create(Interface.class);
 
-            Interface service = retrofit.create(Interface.class);
+                Call<ServerResponse> call = service.post(api_key1, message, mobile_no, sender_id);
 
-            Call<ServerResponse> call = service.post(api_key1, message, mobile_no, senderId);
-
-            call.enqueue(new Callback<ServerResponse>() {
-                @Override
-                public void onResponse(Call<ServerResponse> call, Response<ServerResponse> response) {
-                    // response.isSuccessful() is true if the response code is 2xx
-                    if (response.isSuccessful()) {
-                        Log.e(TAG, response.body().toString());
-                        mobtextingInterface.onResponse(response.body());
-                    } else {
-                        try {
-                            Converter<ResponseBody, ModelError> errorConverter = retrofit.responseBodyConverter(ModelError.class, new Annotation[0]);
-                            ModelError error = errorConverter.convert(response.errorBody());
-                            mobtextingInterface.onError(error);
-                        } catch (IOException e) {
-                            e.printStackTrace();
-                            mobtextingInterface.onError(new ModelError("500", "Something Went Wrong!", "Check your internet connection!/parsing Json exception"));
+                call.enqueue(new Callback<ServerResponse>() {
+                    @Override
+                    public void onResponse(Call<ServerResponse> call, Response<ServerResponse> response) {
+                        // response.isSuccessful() is true if the response code is 2xx
+                        if (response.isSuccessful()) {
+                            Log.e(TAG, response.body().toString());
+                            mobtextingInterface.onResponse(response.body());
+                        } else {
+                            try {
+                                Converter<ResponseBody, ModelError> errorConverter = retrofit.responseBodyConverter(ModelError.class, new Annotation[0]);
+                                ModelError error = errorConverter.convert(response.errorBody());
+                                mobtextingInterface.onError(error);
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                                mobtextingInterface.onError(new ModelError("500", "Something Went Wrong!", "Check your internet connection!/parsing Json exception"));
+                            }
                         }
                     }
-                }
 
-                @Override
-                public void onFailure(Call<ServerResponse> call, Throwable t) {
-                    mobtextingInterface.onError(new ModelError("500", "No Internet", "Check your internet connection!"));
-                }
-            });
+                    @Override
+                    public void onFailure(Call<ServerResponse> call, Throwable t) {
+                        mobtextingInterface.onError(new ModelError("500", "No Internet", "Check your internet connection!"));
+                    }
+                });
+            }else{
+                mobtextingInterface.onError(new ModelError("100", "Sender ID", "Dear developer. Don't forget to configure <meta-data android:name=\"mobtexting.sender_id\" android:value=\"testValue\"/> in your AndroidManifest.xml file."));
+            }
         }else{
             mobtextingInterface.onError(new ModelError("100", "API key", "Dear developer. Don't forget to configure <meta-data android:name=\"mobtexting.api_key\" android:value=\"testValue\"/> in your AndroidManifest.xml file."));
         }
